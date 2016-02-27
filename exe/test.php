6548d5d330efbefa9c9ddeff3d75b69c552a533a
SQLayer <linguri@sqlayer.com>
2016-02-26 17:19:12 -0700

    Change tabs to spaces.

--- exe/test.php
+++ exe/test.php
@@ -10,31 +10,31 @@ require_once(dirname(__DIR__).DIRECTORY_SEPARATOR.'ini.php');
  */
 function testSQLayer() {
 
-	$start = microtime(true);
+    $start = microtime(true);
 
-	echo '--------------TEST SQLAYER---------------'.PHP_EOL;
+    echo '--------------TEST SQLAYER---------------'.PHP_EOL;
 
-	/** access table singleton **/
+    /** access table singleton **/
     $example = SQLayerExampleTable::tbl();
 
     $example->createTable();
     
-	echo '--------------TEST INSERT---------------'.PHP_EOL;
+    echo '--------------TEST INSERT---------------'.PHP_EOL;
 
-	/** test explicit insertion with key **/
-	$example->insertRec(array(1,'AA','Alcoa Inc'));
+    /** test explicit insertion with key **/
+    $example->insertRec(array(1,'AA','Alcoa Inc'));
 
-	/** test keyless insert **/
-	$insert_id = $example->insertRec(array('BA','Boeing Co'));
-	echo 'Insert ID was '.$insert_id.PHP_EOL;
+    /** test keyless insert **/
+    $insert_id = $example->insertRec(array('BA','Boeing Co'));
+    echo 'Insert ID was '.$insert_id.PHP_EOL;
 
-	/** test insert with commas **/
-	$example->insertRec(array(3,'CAT','Caterpillar, Inc'));
+    /** test insert with commas **/
+    $example->insertRec(array(3,'CAT','Caterpillar, Inc'));
 
-	echo '--------------TEST ALLRECS---------------'.PHP_EOL;
+    echo '--------------TEST ALLRECS---------------'.PHP_EOL;
 
-	/** print table records **/
-	print_r($example->allRecs());
+    /** print table records **/
+    print_r($example->allRecs());
 
 }
 
